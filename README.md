afe91ae0ef48ecceede02e80af5536f81db3ed68
Paul Water <p.r.water@setbased.nl>
2022-04-30 07:57:42 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,30 @@
+# PhpPlaisio: Page
+
+<table>
+<thead>
+<tr>
+<th>Social</th>
+<th>Legal</th>
+<th>Release</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>
+<a href="https://gitter.im/PhpPlaisio/PhpPlaisio"><img src="https://badges.gitter.im/PhpPlaisio/PhpPlaisio.svg" alt="Gitter"/></a>
+</td>
+<td>
+<a href="https://packagist.org/packages/plaisio/db-page"><img src="https://poser.pugx.org/plaisio/db-page/license" alt="License"/></a>
+</td>
+<td>
+<a href="https://packagist.org/packages/plaisio/db-page"><img src="https://poser.pugx.org/plaisio/db-page/v/stable" alt="Latest Stable Version"/></a>
+</td>
+</tr>
+</tbody>
+</table>
+
+Provides the SQL scripts for the table tables.
+
+#  License
+
+This project is licensed under the MIT license.
---
+++ lib/ddl/0100_create_tables.sql
@@ -0,0 +1,145 @@
+/*================================================================================*/
+/* DDL SCRIPT                                                                     */
+/*================================================================================*/
+/*  Title    : PhpPlaisio                                                         */
+/*  FileName : db-page.ecm                                                        */
+/*  Platform : MariaDB 10.x                                                       */
+/*  Version  :                                                                    */
+/*  Date     : Saturday, April 30, 2022                                           */
+/*================================================================================*/
+/*================================================================================*/
+/* CREATE TABLES                                                                  */
+/*================================================================================*/
+
+CREATE TABLE `AUT_PAGE_TAB` (
+  `ptb_id` TINYINT AUTO_INCREMENT NOT NULL,
+  `wrd_id` SMALLINT NOT NULL,
+  `ptb_label` VARCHAR(30) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL,
+  CONSTRAINT `PRIMARY_KEY` PRIMARY KEY (`ptb_id`)
+);
+
+/*
+COMMENT ON TABLE `AUT_PAGE_TAB`
+@deprecated
+*/
+
+CREATE TABLE `ABC_AUTH_PAGE` (
+  `pag_id` SMALLINT AUTO_INCREMENT NOT NULL,
+  `pag_id_org` SMALLINT,
+  `ptb_id` TINYINT,
+  `wrd_id` SMALLINT NOT NULL,
+  `pag_alias` VARCHAR(32),
+  `pag_class` VARCHAR(128) NOT NULL,
+  `pag_label` VARCHAR(128) CHARACTER SET ascii COLLATE ascii_general_ci,
+  `pag_weight` INT,
+  CONSTRAINT `PRIMARY_KEY` PRIMARY KEY (`pag_id`)
+);
+
+/*
+COMMENT ON COLUMN `ABC_AUTH_PAGE`.`pag_id`
+The ID of the page.
+*/
+
+/*
+COMMENT ON COLUMN `ABC_AUTH_PAGE`.`pag_id_org`
+@deprecated
+*/
+
+/*
+COMMENT ON COLUMN `ABC_AUTH_PAGE`.`ptb_id`
+@deprecated
+*/
+
+/*
+COMMENT ON COLUMN `ABC_AUTH_PAGE`.`wrd_id`
+The title of the page.
+*/
+
+/*
+COMMENT ON COLUMN `ABC_AUTH_PAGE`.`pag_alias`
+The URL alias of the page.
+*/
+
+/*
+COMMENT ON COLUMN `ABC_AUTH_PAGE`.`pag_class`
+The PHP class that implementing the page.
+*/
+
+/*
+COMMENT ON COLUMN `ABC_AUTH_PAGE`.`pag_label`
+The label (i.e. PHP constant) of the page.
+*/
+
+/*
+COMMENT ON COLUMN `ABC_AUTH_PAGE`.`pag_weight`
+@deprecated
+*/
+
+CREATE TABLE `ABC_AUTH_PAGE_COMPANY` (
+  `pag_id` SMALLINT NOT NULL,
+  `cmp_id` SMALLINT NOT NULL,
+  `pag_class` VARCHAR(128) NOT NULL,
+  CONSTRAINT `PK_ABC_AUTH_PAGE_COMPANY` PRIMARY KEY (`pag_id`, `cmp_id`)
+);
+
+/*
+COMMENT ON TABLE `ABC_AUTH_PAGE_COMPANY`
+A company specific PHP class implementing a page.
+*/
+
+/*
+COMMENT ON COLUMN `ABC_AUTH_PAGE_COMPANY`.`pag_id`
+The page.
+*/
+
+/*
+COMMENT ON COLUMN `ABC_AUTH_PAGE_COMPANY`.`cmp_id`
+The company.
+*/
+
+/*
+COMMENT ON COLUMN `ABC_AUTH_PAGE_COMPANY`.`pag_class`
+The PHP class implementing the page.
+*/
+
+/*================================================================================*/
+/* CREATE INDEXES                                                                 */
+/*================================================================================*/
+
+CREATE INDEX `IX_AUT_PAGE_TAB1` ON `AUT_PAGE_TAB` (`wrd_id`);
+
+CREATE INDEX `IX_ABC_AUTH_PAGE1` ON `ABC_AUTH_PAGE` (`ptb_id`);
+
+CREATE INDEX `IX_ABC_AUTH_PAGE2` ON `ABC_AUTH_PAGE` (`pag_id_org`);
+
+CREATE INDEX `wrd_id` ON `ABC_AUTH_PAGE` (`wrd_id`);
+
+CREATE INDEX `IX_ABC_AUTH_PAGE_COMPANY2` ON `ABC_AUTH_PAGE_COMPANY` (`cmp_id`);
+
+/*================================================================================*/
+/* CREATE FOREIGN KEYS                                                            */
+/*================================================================================*/
+
+ALTER TABLE `AUT_PAGE_TAB`
+  ADD CONSTRAINT `FK_AUT_PAGE_TAB_ABC_BABEL_WORD`
+  FOREIGN KEY (`wrd_id`) REFERENCES `ABC_BABEL_WORD` (`wrd_id`);
+
+ALTER TABLE `ABC_AUTH_PAGE`
+  ADD CONSTRAINT `FK_ABC_AUTH_PAGE_ABC_BABEL_WORD`
+  FOREIGN KEY (`wrd_id`) REFERENCES `ABC_BABEL_WORD` (`wrd_id`);
+
+ALTER TABLE `ABC_AUTH_PAGE`
+  ADD CONSTRAINT `FK_ABC_AUTH_PAGE_AUT_PAGE_TAB`
+  FOREIGN KEY (`ptb_id`) REFERENCES `AUT_PAGE_TAB` (`ptb_id`);
+
+ALTER TABLE `ABC_AUTH_PAGE`
+  ADD CONSTRAINT `FK_ABC_AUTH_PAGE_ABC_AUTH_PAGE`
+  FOREIGN KEY (`pag_id_org`) REFERENCES `ABC_AUTH_PAGE` (`pag_id`);
+
+ALTER TABLE `ABC_AUTH_PAGE_COMPANY`
+  ADD CONSTRAINT `FK_ABC_AUTH_PAGE_COMPANY_ABC_AUTH_COMPANY`
+  FOREIGN KEY (`cmp_id`) REFERENCES `ABC_AUTH_COMPANY` (`cmp_id`);
+
+ALTER TABLE `ABC_AUTH_PAGE_COMPANY`
+  ADD CONSTRAINT `FK_ABC_AUTH_PAGE_COMPANY_ABC_AUTH_PAGE`
+  FOREIGN KEY (`pag_id`) REFERENCES `ABC_AUTH_PAGE` (`pag_id`);
